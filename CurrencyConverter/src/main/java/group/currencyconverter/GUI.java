b2fb62e6353d9197f208404fa5d2ba0eb9f80f2d
BranTen <62572283+BranTen@users.noreply.github.com>
2020-05-01 19:49:50 -0400

    Add files via upload

--- CurrencyConverter/src/main/java/group/currencyconverter/GUI.java
+++ CurrencyConverter/src/main/java/group/currencyconverter/GUI.java
@@ -13,6 +13,7 @@ import java.util.logging.Logger;
 import javax.swing.plaf.ColorUIResource;
 
 public class GUI extends JFrame implements ActionListener {
+
     // these are the elemtns of the GUI
     JFrame frame;
     JTextField from;
@@ -26,169 +27,249 @@ public class GUI extends JFrame implements ActionListener {
     JComboBox toValue;
     JComboBox fromValue;
     String[] keys;
+    CurrencyDB currDB;
+    Date d;
+    DateFormat dateFormat;
+    GridLayout grid;
+    String[] description;
 
-
-    public GUI() {//creation of window an buttons etc
+    // create database object to populate the comboBox
+    private void createDB() {
         try {
-            UIManager.put("TextField.inactiveBackground", new ColorUIResource(new Color(255, 255, 255)));
-            // create database object to populate the comboBox
-            CurrencyDB currDB = new CurrencyDB();
-            Date d = new java.util.Date(currDB.getLastUpdatedDate() * 1000L);
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");  
-            loadDate = dateFormat.format(d);
-            
-            keys = currDB.getAllCurrencyCodes();
-            
-                    String description[] = new String[keys.length];
-        for(int i =0; i < keys.length; i++){
-            description[i] = currDB.getCurrencyCodeDescription(keys[i].toString()) ;
-                    }
-        
-            
-            //initilising the elements of the GUI
-            convertButton= new JButton("Convert");
-            frame = new JFrame();
-            from = new JTextField();
-            
-            to = new JTextField(16);
-            date = new JTextField();
-            toValue = new JComboBox(description);
-            fromValue = new JComboBox(description);
-            
-            //Sets the from value to USD and to value to EUR by default.
-            toValue.setSelectedItem(currDB.getCurrencyCodeDescription("EUR"));
-            fromValue.setSelectedItem(currDB.getCurrencyCodeDescription("USD"));
-            
-            rate = new JLabel("Test Rate: ");
-            fromLabel = new JLabel(currDB.getCurrencyCodeFromDescription(fromValue.getSelectedItem().toString()));
-            toLabel = new JLabel(currDB.getCurrencyCodeFromDescription(toValue.getSelectedItem().toString()));
-
-            //set the frame layout to grid for simplicity
-            GridLayout grid = new GridLayout(3, 3);
-            frame.setLayout(grid);
-            frame.setSize(1000, 200);
-
-        //starting with first row
-            frame.add(new JLabel("Starting currency: "));
-            frame.add(from);
-            from.addKeyListener(new KeyAdapter() { // this allows for the textfield to only accept numerical values and only one decimal point
-                public void keyPressed(KeyEvent e) {
-                    from.setEditable(true); // this will allow for new values to be typed in if the previous value was not accepted
-                    
-                    if (e.getKeyChar() >= '0' && e.getKeyChar() <= '9' || e.getKeyChar() == '.' ||
-                        e.getKeyCode() == 8 || e.getKeyCode() == 127 || e.getKeyCode() == 10 || 
-                        e.getKeyCode() == 37 || e.getKeyCode() == 39)  { // if values are numerical, decimal point,"backspace", "delete", or "enter"
-                        
-                        if (e.getKeyChar() == '.' && from.getText().contains(".")) {// restricts to only one decimal point by searching if there is one already
-
-                            from.setEditable(false);// frevents character from being typed
-                            JOptionPane.showMessageDialog(frame,
-                                    "only one decimal point is allowed",
-                                    "User error",
-                                    JOptionPane.ERROR_MESSAGE);
-                        } else {
-                        from.setEditable(true); // this allows for the value to be entered
-                        }
-                    } else {               
-                        from.setEditable(false);
-                        JOptionPane.showMessageDialog(frame,
-                                "only numerical values are allowed",
-                                "User error",
-                                JOptionPane.ERROR_MESSAGE);
-                    }
-                }
-            });
-            frame.add(fromValue);
-            frame.add(fromLabel);
-            //this action listener is to update the description of the currency key with what is selected in the combo box
-            fromValue.addActionListener(
-                    new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    fromLabel.setText(currDB.getCurrencyCodeFromDescription(fromValue.getSelectedItem().toString()));
-                }});
-
-        //second row
-            frame.add(new JLabel("Ending currency: "));
-            frame.add(to);
-            frame.add(toValue);
-            to.setEditable(false); // no reason for being editable
-            frame.add(toLabel);
-            toValue.addActionListener(
-                    new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    toLabel.setText(currDB.getCurrencyCodeFromDescription(toValue.getSelectedItem().toString()));
-                }});
-            
-        //third row
-            frame.add(convertButton);
-            frame.add(new JLabel("Last load date: "));
-            frame.add(date);
-            frame.add(rate);
-            date.setEditable(false);
-
-        // finishing touches to the GUI
-
-            convertButton.addActionListener(this);
-                                        JOptionPane.showMessageDialog(frame,
-                                              "Welcome to Currency Converter!\n"
-                                            + "Please start by selecting your starting and ending currency.\n"
-                                            + "Then input the amount you would like to convert.\n"
-                                            + "However only numerical values are allowed and only one decimal point.",
-                                    "Info",
-                                    JOptionPane.INFORMATION_MESSAGE);
-            frame.setTitle("Currency Converter");
-            frame.setVisible(true);
-            frame.addWindowListener(new WindowAdapter() {//---------------------------------added lines of code 
-                public void windowClosing(WindowEvent we) {
-                    System.exit(0);
-                }
-            });
-            frame.setResizable(false);
+            currDB = new CurrencyDB();
         } catch (Exception ex) {
+            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    // creats the date object
+    private void setupDate() {
+        d = new java.util.Date(currDB.getLastUpdatedDate() * 1000L);
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        loadDate = dateFormat.format(d);
+    }
+
+    // This populates the boxes from the hashmap
+    private void populateBoxes() {
+        keys = currDB.getAllCurrencyCodes();
+        description = new String[keys.length];
+
+        for (int i = 0; i < keys.length; i++) {
+            description[i] = currDB.getCurrencyCodeDescription(keys[i].toString());
+        }
+    }
+
+    // initilising the elements of the GUI
+    private void initializeElements() {
+
+        //set the frame layout to grid for simplicity
+        frame = new JFrame();
+        grid = new GridLayout(3, 3);
+        frame.setLayout(grid);
+        frame.setSize(1000, 200);
+
+        from = new JTextField();
+        to = new JTextField();
+        to.setEditable(false); // no reason for being editable
+
+        toValue = new JComboBox(description);
+        fromValue = new JComboBox(description);
+        //Sets the from value to USD and to value to EUR by default.
+        toValue.setSelectedItem(currDB.getCurrencyCodeDescription("EUR"));
+        fromValue.setSelectedItem(currDB.getCurrencyCodeDescription("USD"));
+
+
+
+        fromLabel = new JLabel(getLabel("from"));
+        toLabel = new JLabel(getLabel("to"));
+
+        date = new JTextField();
+        date.setEditable(false);
+        rate = new JLabel("Test Rate: ");
+
+        convertButton = new JButton("Convert");
+        convertButton.addActionListener(this);
+    }
+
+    // adds the elements to the frame to be displayed
+    private void addToFrame() {
+        //starting with first row
+        frame.add(new JLabel("Starting currency: "));
+        frame.add(from);
+        frame.add(fromValue);
+        frame.add(fromLabel);
+        //second row
+        frame.add(new JLabel("Ending currency: "));
+        frame.add(to);
+        frame.add(toValue);
+        frame.add(toLabel);
+        //third row
+        frame.add(convertButton);
+        frame.add(new JLabel("Last load date: "));
+        frame.add(date);
+        frame.add(rate);
+
+        displayMessage("welcome");
+        frame.setTitle("Currency Converter");
+        frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+        frame.setResizable(false);
+
+        UIManager.put("TextField.inactiveBackground", new ColorUIResource(new Color(255, 255, 255)));
+
+    }
+
+    // contains the listeners for the dropboxes as well as the textbox
+    private void addListeners() {
+        //this action listener is to update the description of the currency key with what is selected in the combo box
+        fromValue.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                fromLabel.setText(getLabel("from"));
+            }
+        });
+        toValue.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                toLabel.setText(getLabel("to"));
+            }
+        });
+
+        from.addKeyListener(new KeyAdapter() { // this allows for the textfield to only accept numerical values and only one decimal point
+            public void keyPressed(KeyEvent e) {
+                from.setEditable(true); // this will allow for new values to be typed in if the previous value was not accepted
+
+                if (e.getKeyChar() >= '0' && e.getKeyChar() <= '9' || e.getKeyChar() == '.'
+                        || e.getKeyCode() == 8 || e.getKeyCode() == 127 || e.getKeyCode() == 10
+                        || e.getKeyCode() == 37 || e.getKeyCode() == 39) { // if values are numerical, decimal point,"backspace", "delete", or "enter"
+
+                    if (e.getKeyChar() == '.' && from.getText().contains(".")) {// restricts to only one decimal point by searching if there is one already
+
+                        from.setEditable(false);// frevents character from being typed
+                        displayMessage("decimal");
+
+                    } else {
+                        from.setEditable(true); // this allows for the value to be entered
+                    }
+                } else {
+                    from.setEditable(false);
+                    displayMessage("numerical");
+                }
+            }
+        });
+    }
+
+    // calls private methods to form the GUI a method at a time
+    public GUI() {
+        createDB();
+        setupDate();
+        populateBoxes();
+        initializeElements();
+        addToFrame();
+        addListeners();
+
+    }
+    
     // this is where the GUI does stuff
     public void actionPerformed(ActionEvent event) {
-        // last test for input values
-            String[] a = from.getText().split("[.]");
-            String decimal = a[1];        
-        if(from.getText().equals("") || 0.00 == Double.parseDouble(from.getText())){ // if nothing is entered or if value is equal to 0
-            JOptionPane.showMessageDialog(frame,
-            "Please enter a value greater than 0.00",
-            "User error",
-            JOptionPane.ERROR_MESSAGE);
-        }else
-            if(from.getText().contains(".") && decimal.length() > 2){ // if value has too many digits after the decimal point
-            JOptionPane.showMessageDialog(frame,
-            "Please enter a value with only 2 digits after decimal point",
-            "User error",
-            JOptionPane.ERROR_MESSAGE);
-            }
-        else{
-                //update the load date
-        date.setText(loadDate);
-        from.setEditable(true); // this allows for the textbox to be modifiable again incase if the last value entered locked it
-        //Here is where the conversion takes place
-        try {
-            // Need the database and converion objects to convert
-            CurrencyDB currDB = new CurrencyDB();
+
+        if (inputCheck()) {
             ConversionModule convMod = new ConversionModule(currDB);
-            
+
+            //update the load date
+            date.setText(loadDate);
+            from.setEditable(true); // this allows for the textbox to be modifiable again incase if the last value entered locked it
+            //Here is where the conversion takes place
+
             //find the rate of the two values
-            double testRate = convMod.convertCurrency(fromLabel.getText(),
-                                                      toLabel.getText());
-            
+            double testRate = convMod.convertCurrency(fromLabel.getText(), toLabel.getText());
+
             //just converts the textbox value to a double
             double x = Double.parseDouble(from.getText());
             //update the rate for the user to see
             rate.setText("Test Rate: " + String.format("%.2f", testRate));
             //apply the converted value to the unmodifable textbox for the user to see
-            to.setText (String.format("%.2f", (x * testRate)));
-
-        } catch (Exception ex) { // auto generated
-            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+            to.setText(String.format("%.2f", (x * testRate)));
         }
+    }
+
+    // last test for input values
+    private boolean inputCheck() {
+        boolean result = true;
+
+        String[] a;
+        if (from.getText().equals("") || 0.00 == Double.parseDouble(from.getText())) { // if nothing is entered or if value is equal to 0
+            displayMessage("value");
+            result = false;
+        }
+        if (from.getText().contains(".")) { // if value has too many digits after the decimal point
+            a = from.getText().split("[.]");
+            if (a.length == 2) {
+                if (a[1].length() > 2) {
+                    displayMessage("digit");
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
     
-        }}    
+    // Gets the label to match the combobox selection
+    private String getLabel(String origin) {
+        String label = "";
+        if (origin.equalsIgnoreCase("from")) {
+            label = currDB.getCurrencyCodeFromDescription(fromValue.getSelectedItem().toString());
+        }
+        if (origin.equalsIgnoreCase("to")) {
+            label = currDB.getCurrencyCodeFromDescription(toValue.getSelectedItem().toString());
+        }
+        return label;
+    }
+
+    // this is just a method to keep the JOptionPane messages
+    private void displayMessage(String type) {
+        switch (type) {
+            case "welcome":
+                JOptionPane.showMessageDialog(frame,
+                        "Welcome to Currency Converter!\n"
+                        + "Please start by selecting your starting and ending currency.\n"
+                        + "Then input the amount you would like to convert.\n"
+                        + "However only numerical values are allowed and only one decimal point.\n"
+                        + "Once you are ready click the convert button and see your conversion.",
+                        "Info",
+                        JOptionPane.INFORMATION_MESSAGE);
+                break;
+
+            case "decimal":
+                JOptionPane.showMessageDialog(frame,
+                        "only one decimal point is allowed",
+                        "User error",
+                        JOptionPane.ERROR_MESSAGE);
+                break;
+
+            case "numerical":
+                JOptionPane.showMessageDialog(frame,
+                        "only numerical values are allowed",
+                        "User error",
+                        JOptionPane.ERROR_MESSAGE);
+                break;
+
+            case "value":
+                JOptionPane.showMessageDialog(frame,
+                        "Please enter a value greater than 0.00",
+                        "User error",
+                        JOptionPane.ERROR_MESSAGE);
+                break;
+
+            case "digit":
+                JOptionPane.showMessageDialog(frame,
+                        "Please enter a value with only 2 digits after decimal point",
+                        "User error",
+                        JOptionPane.ERROR_MESSAGE);
+                break;
+        }
+    }
+
 }
